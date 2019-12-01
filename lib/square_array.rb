0cb42ea21f39142f9407dde1766084f80371fa5d
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-01 14:52:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ new_arr = []
   while array[counter] do
     new_array << (array[counter] ** 2)
     counter += 1
-    return square_array(new_array)
+    return square_array(new_arr)
   end
   
 end
